9f26fb473ac8e373ca5b7859600c121991a52d0f
sofia tselen <sofiatselenx@gmail.com>
2024-05-06 19:16:33 +0300

    update reviews

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,38 +1,53 @@
-import { useEffect, useState } from "react";
-import { ApiMovieRewiews } from "../ApiService/ApiService";
-import { useParams } from "react-router-dom";
-import css from "./MovieReviews.module.css";
-const MovieReviews = () => {
-  const { movieId } = useParams();
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const results = await ApiMovieRewiews(movieId);
-        setRewiews(results);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchData();
-  }, [movieId, rewiews]);
-  return (
-    <>
-      {rewiews.length === 0 ? (
-        <p className={css.noResult}>
-          We do not have any rewiews for this movies😢
-        </p>
-      ) : (
-        <ul className={css.rewiewsList}>
-          {rewiews.results.map((item) => (
-            <li key={item.id} className={css.rewiewsItem}>
-              <p>{item.author}</p>
-              <p>{item.content}</p>
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
-  );
-};
+import css from './MovieReviews.module.css';
+import { useState, useEffect } from 'react';
+import { getReviews } from '../ApiService/ApiService';
+import { useParams } from 'react-router-dom';
+import Loader from '../Loader/Loader';
 
-export default MovieReviews;
+export default function MoviesReviews( ) {
+  const {movie} = useParams();
+  const [isloading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [reviews, setReviews] = useState([]);
+  let movieId;
+  if (movie.includes(":")){
+     movieId = movie.slice(1);
+  } else {
+      movieId = movie;
+  }
+
+  useEffect(() =>{
+    async function getFetchmovies() {
+        try {
+            setError(false);
+            setIsLoading(true);
+        const data = await getReviews(movieId)
+        setReviews(data.result.results);
+        console.log(data.result.results);
+    } catch (error) {
+        setError(true);
+    } finally {
+        setIsLoading(false)
+    }
+    }
+    getFetchmovies();    
+}, [movieId])
+
+return (<>
+  {isloading&& <Loader/>}
+       {error && <Error />}
+       {!isloading && !error  &&(
+           <ul>
+               {reviews.length > 0 ?
+               reviews.map(review => (
+                   <li key={review.id} className={css.review}>
+                       <h4>{review.author}</h4>
+                       <p>{review.content}</p>
+
+
+                   </li>
+               )) : (<p>we dont have reviews</p>)}
+           </ul>
+       )}
+   </>)
+}
